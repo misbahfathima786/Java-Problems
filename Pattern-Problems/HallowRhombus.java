ff0e99043c40e2f2adf15b137314320b68d28c3a
misbahfathima786 <misbahfathima785@gmail.com>
2026-08-16 12:32:53 +0530

    Update HallowRhombus.java

--- Pattern-Problems/HallowRhombus.java
+++ Pattern-Problems/HallowRhombus.java
@@ -6,7 +6,7 @@ class HallowRhombus {
         int n = sc.nextInt();
         for(int i = 1;i <= n;i++) {
             for(int j = 1;j <= n; j++) {
-                if(i==1 || i==n || i==n || j==1 || j==n)
+                if(i==1 || i==n || j==1 || j==n)
                     System.out.print("* ");
                 else
                     System.out.print("  ");
